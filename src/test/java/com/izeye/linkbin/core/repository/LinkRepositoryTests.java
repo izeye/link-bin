8507c450f4d901df862f0e85b586ab587c7a47ea
Johnny Lim <izeye@naver.com>
2017-02-06 23:47:53 +0900

    Upgrade to Spring Boot 1.5.1.RELEASE

--- src/test/java/com/izeye/linkbin/core/repository/LinkRepositoryTests.java
+++ src/test/java/com/izeye/linkbin/core/repository/LinkRepositoryTests.java
@@ -1,11 +1,10 @@
 package com.izeye.linkbin.core.repository;
 
-import com.izeye.linkbin.Application;
 import com.izeye.linkbin.core.domain.Link;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +15,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
- * Created by izeye on 15. 12. 6..
+ * Tests for {@link LinkRepository}.
+ *
+ * @author Johnny Lim
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@SpringBootTest
 public class LinkRepositoryTests {
 	
 	@Autowired
